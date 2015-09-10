bc60848462a612519159b4d85d7c07adf58627fc
Oskar Jung <oskar@mbr-targeting.com>
2015-09-10 17:20:33 +1200

    Refactor persist
    Now takes a debounce interval as well as a store object that has to
    implement `setItem(key, value)` and `getItem(key)` functions.

--- app/scripts/persist.js
+++ app/scripts/persist.js
@@ -1,40 +1,30 @@
 import Immutable from 'immutable';
 import Kefir from 'kefir';
 
-const localStorage = window.localStorage;
-
-const persist = next => (reducer, initalState) => {
-  const localState = rehydrate(localStorage.getItem('redux-store'));
+const persist = (storage, debounceInterval) => next => (reducer, initalState) => {
+  const localState = rehydrate(storage.getItem('redux-store'));
   const state = localState.merge(initalState);
   const store = next(reducer, state);
 
   dataStream(store)
-    .debounce(250)
+    .debounce(debounceInterval)
     .map(serialize)
-    .onValue(saveToLocalStorage);
+    .onValue(saveToStorage(storage));
 
   return {...store};
 }
 export default persist;
 
 const rehydrate = state => {
-  return Immutable.Map({
-    data: Immutable.fromJS(JSON.parse(state)),
-    ui: Immutable.Map(),
-  });
+  const localState = state && JSON.parse(state) || {};
+  return Immutable.fromJS(localState);
 };
 
 const serialize = state => JSON.stringify(state.toJS());
 
-const saveToLocalStorage = state => localStorage.setItem('redux-store', state);
+const saveToStorage = storage => state => storage.setItem('redux-store', state);
 
-const dataStream = store => {
-  return Kefir.stream(listener(store));
-};
+const dataStream = store => Kefir.stream(listener(store));
 
-const listener = store => emitter => {
-  store.subscribe(() => {
-    const state = store.getState();
-    emitter.emit(state.get('data'));
-  });
-};
+const listener = store => emitter =>
+  store.subscribe(() => emitter.emit(store.getState()));
